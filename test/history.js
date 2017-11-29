01b1936d0bfa6d723456cabf8fa82d675bd99d10
Stephen Whitmore <sww@eight45.net>
2017-11-28 20:25:52 -0800

    test: add tests for 2 feeds /w clock conflicts

--- test/history.js
+++ test/history.js
@@ -73,8 +73,56 @@ tape('mutliple values: same key', function (t) {
   })
 })
 
-// TODO: multiple feeds (/wo conflict)
-// TODO: multiple feeds (/w conflict in toposort)
+tape('2 feeds', function (t) {
+  create.two(function (a, b) {
+    a.put('/a', 'a', function () {
+      b.put('/b', '12', function () {
+        replicate(a, b, validate)
+      })
+    })
+
+    function validate () {
+      var rs = b.createHistoryStream('/a')
+      collect(rs, function (err, actual) {
+        t.error(err, 'no error')
+        t.equals(actual.length, 3)
+        t.equals(actual[0].feed + ',' + actual[0].seq, '0,0')
+        t.equals(actual[1].feed + ',' + actual[1].seq, '0,1')
+        t.equals(actual[2].feed + ',' + actual[2].seq, '1,0')
+        t.end()
+      })
+    }
+  })
+})
+
+tape('2 feeds: same key', function (t) {
+  create.two(function (a, b) {
+    a.put('/a', 'a', function () {
+      b.put('/a', 'b', function () {
+        replicate(a, b, function () {
+          a.put('/a', 'c', function () {
+            replicate(a, b, function () {
+              validate()
+            })
+          })
+        })
+      })
+    })
+
+    function validate () {
+      var rs = a.createHistoryStream('/a')
+      collect(rs, function (err, actual) {
+        t.error(err, 'no error')
+        t.equals(actual.length, 4)
+        t.equals(actual[0].feed + ',' + actual[0].seq, '0,0')
+        t.equals(actual[1].feed + ',' + actual[1].seq, '0,1')
+        t.equals(actual[2].feed + ',' + actual[2].seq, '1,0')
+        t.equals(actual[3].feed + ',' + actual[3].seq, '0,2')
+        t.end()
+      })
+    }
+  })
+})
 
 function collect (stream, cb) {
   var res = []
